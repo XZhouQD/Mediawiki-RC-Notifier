9c23a8c0a7af78b4a740f69445ffbacf8a785818
Xiaowei-Zhou <xiaowei.zhou@unsw.edu.au>
2020-08-17 22:24:26 +0800

    0.2.0-Alpha
    RC Feed does not provide support for new page, move to dumb api query

--- mediawiki_rc_lookup.py
+++ mediawiki_rc_lookup.py
@@ -7,18 +7,53 @@ A Nonebot Plugin
 use with configurations
 
 Version:
-0.1.0-Beta
+0.2.0-Alpha
 """
 
 # Please configure this before using:
 API_PATH='https://your-domain.com/wiki/api.php'
 SITE_NAME='Your sitename'
 
+class NotificationCache:
+    """The notification cache"""
+    def __init__(self):
+        self.cache = []
+    
+    async def fetch(self):
+        result_list = await fetch_rc(20)
+        diff = list(set(result_list).difference(set(self.cache)))
+        self.cache = list(set(self.cache).union(set(result_list)))
+        return ''.join(diff)
+        
+notification_cache = NotificationCache()
+notification_cache.fetch()
+# Please configure this before using:
+TARGET_ID=[11111111,222222222] # notice target user/group id
+PRIVATE=False #user: true, group: false
+
+@nonebot.scheduler.scheduled_job('interval', seconds=10)
+async def _():
+    bot = nonebot.get_bot()
+    message = notification_cache.fetch()
+    if len(message) != 0:
+        for target in TARGET_ID:
+            if PRIVATE:
+                try:
+                    await bot.send_private_msg(user_id=target, message=message)
+                except CQHttpError:
+                    pass
+            else:
+                try:
+                    await bot.send_group_msg(group_id=target, message=message)
+                except CQHttpError:
+                    pass
+
 # manual lookup
 @on_command('rc', aliases=('最近更改'), permission=permission.GROUP_ADMIN, only_to_me=False)
 async def rc(session: CommandSession):
     rclimit = session.get('rclimit')
-    result = await fetch_rc(rclimit)
+    result_list = await fetch_rc(rclimit)
+    result = ''.join(result_list)
     await session.send(result)
 
 @rc.args_parser
@@ -36,6 +71,7 @@ async def fetch_rc(rclimit: int) -> str:
     j = r.json()
     result_list = [f'Recent Changes on {SITE_NAME}:\n']
     for rc_item in j['query']['recentchanges']:
-        result_list.append(f'{rc_item["rcid"]}: {rc_item["title"]}\n')
-    result = ''.join(result_list)
-    return result
+        if rc_item["type"] not in ['edit', 'new']:
+            continue
+        result_list.append(f'操作{rc_item["rcid"]}: {"新页面" if rc_item["type"] == "new" else "修改页面"} {rc_item["title"]}\n')
+    return result_list
