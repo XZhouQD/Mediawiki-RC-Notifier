5cf546a979f0e05a72a5fc82fce86f86859cad3d
Xiaowei-Zhou <xiaowei.zhou@unsw.edu.au>
2020-08-21 19:48:06 +0800

    Update mediawiki_rc_notifier.py

--- mediawiki_rc_notifier.py
+++ mediawiki_rc_notifier.py
@@ -3,13 +3,14 @@ from aiocqhttp.exceptions import Error as CQHttpError
 from json import loads
 from socket import socket, AF_INET, SOCK_DGRAM
 from threading import Thread
+import socketserver
 
 """
 Mediawiki RecentChange(RC) Notifier
 A Nonebot Plugin
 
 Version:
-0.3.0-Beta
+0.4.0-Alpha
 """
 
 # Configurable
@@ -19,28 +20,44 @@ SITE_NAME='YOUR SITE NAME'
 TARGET_ID=[11111111,222222222] # notice target user/group id
 PRIVATE=False # user: True, group: False
 
-class udpThread(Thread):
-    def __init__(self, ip, port, cache):
+class Cache():
+    def __init__(self):
+        self.queue = []
+
+    async def fetch(self):
+        n_list = self.queue.copy()
+        self.queue = []
+        await notify(n_list)
+
+    def push(self, item):
+        self.queue.append(item)
+
+cache = Cache()
+
+class UdpHandler(socketserver.BaseRequestHandler):
+    def __init__(self, cache):
+        socketserver.BaseRequestHandler.__init__(self)
+
+    def handle(self):
+        global cache
+        data, sock = self.request
+        data = loads(data.decode())
+        if data["type"] not in ["edit", "new"]:
+            continue
+        message = f'{SITE_NAME}有条目更新! {data["id"]}: {"新建" if data["type"] == "new" else "修改"}页面 {data["title"]}'
+        nonebot.log.logger.info('[MW RC No]'+message)
+        cache.push(message)
+
+class UdpThread(Thread):
+    def __init__(self, ip, port):
         Thread.__init__(self)
         self.address = (ip, port)
-        self.server = socket(AF_INET, SOCK_DGRAM)
-        self.server.bind(self.address)
+        self.server = socketserver.ThreadingUDPServer(address, UdpHandler)
         nonebot.log.logger.info(f'[MW RC NO]Your UDP Server binds to {self.address}')
-        self.cache = cache
 
     def run(self):
         nonebot.log.logger.info("[MW RC No]Starting UDP Server Thread...")
-        while True:
-            try:
-                data, addr = self.server.recvfrom(2048)
-                data = loads(data.decode())
-                if data["type"] not in ["edit", "new"]:
-                    continue
-                message = f'{SITE_NAME}有条目更新! {data["id"]}: {"新建" if data["type"] == "new" else "修改"}页面 {data["title"]}'
-                nonebot.log.logger.info('[MW RC No]'+message)
-                self.cache.push(message)
-            except:
-                continue
+        self.server.serve_forever()
 
 async def notify(msg_list):
     if (len(msg_list)) == 0:
@@ -61,24 +78,10 @@ async def notify(msg_list):
             except CQHttpError:
                 pass
 
-class Cache():
-    def __init__(self):
-        self.queue = []
-
-    async def fetch(self):
-        n_list = self.queue.copy()
-        self.queue = []
-        await notify(n_list)
-
-    def push(self, item):
-        self.queue.append(item)
-
-cache = Cache()
-
 @nonebot.on_startup
 async def startup():
     nonebot.log.logger.info('[MW RC No]Thank you for using Mediawiki RC Notifier!')
-    udp_server_thread = udpThread(IP, PORT, cache)
+    udp_server_thread = udpThread(IP, PORT)
     udp_server_thread.start()
 
 @nonebot.scheduler.scheduled_job('interval', seconds=30)
